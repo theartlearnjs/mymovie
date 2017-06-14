7fb6ed482b8b50a79e1b597f2079e3706a76eedc
Wang Bin <BG296844@800best.net>
2017-06-14 16:04:23 +0800

    add mongoose

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router()
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 router.get('/', function(req, res) {
-    res.render('index', { title: 'Express' })
+    res.render('index', { title: '电影' })
 })
 
 module.exports = router
